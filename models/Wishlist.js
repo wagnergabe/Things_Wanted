ef29946a97c8c430c9ce5412031e5f1680a94435
Tricia <tricianghiem@gmail.com>
2022-08-04 23:52:39 -0500

    created js model for wishlist table and data

---
+++ models/Wishlist.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Wishlist extends Model {}
+
+Wishlist.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    wishlist_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    event: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    item_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.STRING,
+    },
+    url: {
+      type: DataTypes.TEXT,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "wishlist",
+  }
+);
+
+module.exports = Wishlist;
