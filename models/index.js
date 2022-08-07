2c836c86c8603f87e1b29607e417c77509b33e12
GSvenny <gabrielswagner7@gmail.com>
2022-08-06 21:05:53 -0500

    update model relationships

--- models/index.js
+++ models/index.js
@@ -1,24 +1,14 @@
 const Users = require("./Users");
 const Wishlist = require("./Wishlist");
 
-// User.hasMany(Wish, {
-//     foreignKey: 'user_list',
-//     onDelete: 'CASCADE'
-// })
+Users.hasMany(Wishlist, {
+    foreignKey: 'user_list',
+    onDelete: 'CASCADE'
+});
 
-// User.hasMany(Comment, {
-//     foreignKey: "user_list",
-//     onDelete: 'CASCADE'
-// })
-
-// Comment.belongsTo(User, {
-//     foreignKey: "user_list",
-//     onDelete: 'CASCADE'
-// })
-
-// Wish.belongsTo(User, {
-//     foreignKey: 'user_list',
-//     onDelete: 'CASCADE'
-// })
+Wishlist.belongsTo(Users, {
+    foreignKey: 'user_list',
+    onDelete: 'CASCADE'
+})
 
 module.exports = { Users, Wishlist };
