b7f1ef1708be6d128c306efb655d767d1b25c2d0
GSvenny <gabrielswagner7@gmail.com>
2022-08-08 20:41:36 -0500

    fixed router link, MVC more proper and organized

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -16,27 +16,6 @@ router.get('/',  (req, res) => {
   })
 })
 
-// Routes for wishlist
-
-router.get("/wishlist", function (req, res) {
-  res.render("partials/wishlist", {qs: req.query});
-});
-
-router.post("/wishlist", urlencodedParser, function (req, res) {
-Post.create({
-  wishlist_name: req.body.wishlist_name,
-  event: req.body.event,
-  item_name: req.body.item_name,
-  category: req.body.category,
-  url: req.body.url
-})
-  .then(dbPostData => res.json(dbPostData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err)
-  });
-})
-
 
 // Login route
 router.get('/login', (req, res) => {
