d6adaf30ee210da49516189f3bf89e93e4dedb62
GSvenny <gabrielswagner7@gmail.com>
2022-08-10 19:30:24 -0500

    fixed api route

--- controllers/gift-routes.js
+++ controllers/gift-routes.js
@@ -3,15 +3,26 @@ const sequelize = require('../config/connection');
 const { User, Wishlist} = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get("/", (req , res) => {
-    Wishlist.findAll()
-    .then(dbWishlistData => res.json(dbWishlistData))
-    .catch(err => {
+router.get('/', (req, res) => {
+    Wishlist.findAll({
+      attributes: [
+        'id',
+        'wishlist_name',
+        'event_name',
+        'item_name',
+        "category",
+        "url",
+      ],
+    })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('gifts', {posts} );
+      })
+      .catch(err => {
         console.log(err);
         res.status(500).json(err);
-    })
-    res.render("gifts")
-})
+      });
+  });
 
 //get single gift
 
