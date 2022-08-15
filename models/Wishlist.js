22e06e63ce96fe4867307fb88dd8db1740e796ab
GSvenny <gabrielswagner7@gmail.com>
2022-08-14 20:03:40 -0500

    change models

--- models/Wishlist.js
+++ models/Wishlist.js
@@ -29,18 +29,6 @@ Wishlist.init(
     url: {
       type: DataTypes.TEXT,
     },
-    user_id: {
-      type: DataTypes.STRING,
-      references: {
-        model: 'user',
-        key: 'id'
-        //foreignKey found in Index
-      }
-    }
-    // This is what links the two models together, thats all this is here for, dont try to combine attributes in this page (like i did). Pick the attributes you want to combine in the controller/routes
-    // table merge example in controllers/gift-routes.js
-  },
-  {
     sequelize,
     freezeTableName: true,
     underscored: true,
